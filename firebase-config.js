32f0dc4f16a2ece86b675a6b01adfbba0269f3fb
@joseavera <69088516+joseavera@users.noreply.github.com>
2025-06-12 14:44:33 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,11 +4,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 
   const firebaseConfig = {
     apiKey: "AIzaSyBZEVISZuRfn1iC3be5b7xTMQiCZubXyTU",
-    authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "bdnosql-de619",
-    storageBucket: "bdnosql-de619.firebasestorage.app",
-    messagingSenderId: "519369652548",
-    appId: "1:519369652548:web:260c2b45275f5a0ff5ff11",
+75f5a0ff5ff11",
     measurementId: "G-1FYEJLG220"
   };
 
